6121f9defcca405d15c5468ec0d551ddabb7e5f9
JoakinGC <joakingcontreras2@gmail.com>
2024-09-06 13:37:18 +0200

    add routes

---
+++ config/routes.py
@@ -0,0 +1,8 @@
+import os
+
+RUTAS = {
+    "search_ruta": os.path.join(".", "modulos", "search", "search.py"),
+    "web_scrapping": "C:/Users/USUARIO/OneDrive - fempa.es/Escritorio/ratonEyes/modulos/webScrapping/web.py",
+}
+
+
